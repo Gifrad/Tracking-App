ee3b626965eb2c0cac76ea00f5be68146a5509c0
Gilang Fitra Ramadhana <gifrad98@gmail.com>
2024-01-31 23:13:01 +0700

    Slicing Splash Page

---
+++ lib/shared/theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+Color blueColor = const Color(0xff257EB5);
+Color whiteColor = const Color(0xffFFFFFF);
+Color greenColor = const Color(0xff16A825);
+Color borderGreenColor = const Color(0xff0FA958);
+Color redColor = const Color(0xffEA2B01);
+Color blackColor = const Color(0xff000000);
+Color blackBodyTextColor = const Color(0xff052437);
+Color greySecondaryColor = const Color(0xff697C87);
+Color greyThirdColor = const Color(0xff6B6464);
+Color blueTextColor = const Color(0xff0082D2);
+Color blueButtonColor = const Color(0xff0F87DA);
